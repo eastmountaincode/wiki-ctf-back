2feaa5bef4b4f93719022056a281acd7d80b5818
Andrew Boylan <andreweboylan@gmail.com>
2025-07-13 03:49:12 +0000

    title moving

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const http = require('http');
 const { Server } = require('socket.io');
 
@@ -13,23 +12,44 @@ const io = new Server(server, {
     }
 });
 
-// Ledger of all avatars: { [socket.id]: { x, y, color, label, zoneIndex } }
+// Ledger of all avatars: { [socket.id]: { ...avatarData } }
 const avatars = {};
+
+// --- Ledger of movable titles ---
+// Match your client initial positions/config!
+const titles = {
+    sphagnopsida: {
+        id: 'sphagnopsida',
+        x: 254,
+        y: 84,
+        currentZoneIndex: 1,
+        carriedBy: null, // socket.id if carried
+        teamColor: null,
+    },
+    takakia: {
+        id: 'takakia',
+        x: 205,
+        y: 80,
+        currentZoneIndex: 2,
+        carriedBy: null,
+        teamColor: null,
+    },
+};
 
 // Handle new connections
 io.on('connection', (socket) => {
     console.log('Client connected:', socket.id);
 
-    // Client joins with initial avatar state
+    // --- Avatars ---
     socket.on('join', (data) => {
         avatars[socket.id] = {
             ...data,
             id: socket.id // so frontend can identify self
         };
         io.emit('avatars', Object.values(avatars));
+        io.emit('titles', Object.values(titles)); // Send titles to newly connected client
     });
 
-    // Client sends movement/state updates
     socket.on('move', (data) => {
         if (avatars[socket.id]) {
             avatars[socket.id] = {
@@ -40,10 +60,40 @@ io.on('connection', (socket) => {
         }
     });
 
-    // Handle disconnect
+    // --- Titles: position/ownership update ---
+    socket.on('titleMove', (data) => {
+        const { titleId, x, y, currentZoneIndex, carriedBy, teamColor } = data;
+
+        if (titles[titleId]) {
+            titles[titleId] = {
+                ...titles[titleId],
+                x,
+                y,
+                currentZoneIndex,
+                carriedBy, // should be a socket.id or null
+                teamColor,
+            };
+            io.emit('titles', Object.values(titles));
+        }
+    });
+
+    // --- Clean up on disconnect ---
     socket.on('disconnect', () => {
         delete avatars[socket.id];
         io.emit('avatars', Object.values(avatars));
+
+        // Drop any titles this player was carrying
+        let updated = false;
+        Object.values(titles).forEach(title => {
+            if (title.carriedBy === socket.id) {
+                title.carriedBy = null;
+                updated = true;
+            }
+        });
+        if (updated) {
+            io.emit('titles', Object.values(titles));
+        }
+
         console.log('Client disconnected:', socket.id);
     });
 });
